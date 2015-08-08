00ae86454c9784b281b97ce378fce40bc0c13724
Rushil Patel <rushil9@gmail.com>
2015-08-08 11:31:17 -0700

    Main Menu done..ish tutorial levels implemented 12 levels so far

---
+++ Proto.spritebuilder/InstructionalPlayScene.swift
@@ -0,0 +1,9 @@
+//
+//  InstructionalLevelScene.swift
+//  Proto
+//
+//  Created by Rushil Patel on 8/7/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
---
+++ Proto.spritebuilder/LevelSelectContent.swift
@@ -0,0 +1,9 @@
+//
+//  LevelSelectContent.swift
+//  Proto
+//
+//  Created by Rushil Patel on 8/7/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
